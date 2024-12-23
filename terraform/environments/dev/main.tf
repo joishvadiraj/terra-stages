db0ab0e42022fad84978d2fb64566acb84494c7e
Vadiraj Joish <joish.vadiraj@gmail.com>
2024-12-23 10:18:11 +0100

    CI Update

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "dev-vpc" {
-    cidr_block = var.vpc_cidr_block
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    tags = {
-        Name = "dev-vpc"
-    }
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "dev-vpc"
+  }
 
-  
+
 }
